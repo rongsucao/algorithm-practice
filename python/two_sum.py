a4435699aac3dbe76f3a0b91647005edaf550952
rongsucao <caorongsu@gmail.com>
2026-01-11 14:56:33 -0800

    Add LC 1 Two Sum

---
+++ python/two_sum.py
@@ -0,0 +1,10 @@
+# LC 1. Two Sum
+# https://leetcode.com/problems/two-sum/
+
+def twoSum(nums, target):
+    seen = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in seen:
+            return [seen[diff], i]
+        seen[num] = i
