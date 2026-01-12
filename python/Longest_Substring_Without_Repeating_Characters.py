e6d47703edbc5f9137b1846457848dbf39ceb1fa
rongsucao <caorongsu@gmail.com>
2026-01-11 19:04:09 -0800

    Add LC 3 Longest Substring

--- python/Longest_Substring_Without_Repeating_Characters.py
+++ python/Longest_Substring_Without_Repeating_Characters.py
@@ -1,17 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s):
+        char_index = {}
         left = 0
-        subarry = {}
         ans = 0
-        for i, str in enumerate(s):
-            if str not in subarry:
-                subarry[str] = i
-                ans = max(ans, i + 1 -left)
-            else:
-                left = subarry.get(str) + 1
-                subarry = {}
-                for j in range(left, i+1):
-                    subarry[s[j]] = j
-                subarry[str] = i
         
+        for i, c in enumerate(s):
+            if c in char_index and char_index[c] > left:
+                left = char_index[c] + 1
+                
+            char_index[c] = i
+            ans = max(ans, i - left + 1)
+            
         return ans
