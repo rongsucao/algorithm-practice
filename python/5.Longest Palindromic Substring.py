e3f02b13fd501d861a8eadefa0907b0ecd1210c8
Rongsu Cao <123025544+rongsucao@users.noreply.github.com>
2026-01-11 23:05:55 -0800

    Clean up code by removing comments and test cases
    Removed comments and test cases for clarity.

--- python/5.Longest Palindromic Substring.py
+++ python/5.Longest Palindromic Substring.py
@@ -1,11 +1,3 @@
-#
-# @lc app=leetcode id=5 lang=python3
-# @lcpr version=30306
-#
-# [5] Longest Palindromic Substring
-#
-
-# @lc code=start
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         def expand(l,r):
@@ -27,19 +19,3 @@ class Solution:
         return res        
         
             
-            
-# @lc code=end
-
-
-
-#
-# @lcpr case=start
-# "babad"\n
-# @lcpr case=end
-
-# @lcpr case=start
-# "cbbd"\n
-# @lcpr case=end
-
-#
-
